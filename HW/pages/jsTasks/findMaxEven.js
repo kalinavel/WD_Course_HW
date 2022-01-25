2ee8357ca165050f92f251c4700f40632add5f76
Kalina <92947262+kalinavel@users.noreply.github.com>
2022-01-25 17:16:11 +0200

    Add files via upload

--- HW/pages/jsTasks/findMaxEven.js
+++ HW/pages/jsTasks/findMaxEven.js
@@ -7,13 +7,14 @@ let numbers = [2, -4, 5, 3, 9, 0, 1];
 let max = findMaxEven(numbers);
 
 function findMaxEven(numbers) {
+    let max = numbers[0];
     for (let i = 0; i < numbers.length; i++) {
         const num = numbers[i];
-        let max = numbers[numbers.length - 1];
-
         if ((num % 2 == 0) && (num >= max)) {
-            max = num;
-            return console.log(`The max of ${numbers} is ${max}`);
-        }
-    }
-}   
+            max = num; 
+        } 
+    } 
+    return max;
+} 
+
+console.log(`The max of ${numbers} is ${max}`); 
